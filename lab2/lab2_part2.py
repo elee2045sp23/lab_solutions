a8ccafb79c803596ada2cb6e8c681bd9c0114608
kjjohnsen <kjjohnsen@users.noreply.github.com>
2023-02-10 06:40:51 -0500

    added a few comments to lab 2 solution

--- lab2/lab2_part2.py
+++ lab2/lab2_part2.py
@@ -1,15 +1,16 @@
 import serial
-num_trials=3
-s = serial.Serial("COM3",115200)
+num_trials=3 # modify this line to read more lines
+s = serial.Serial("COM3",115200) # this line may need to be modified for your port
 
-
+# our data comes through as string1,string2\n
+# we'll wait for a line, then write it directly to the file
 with open("reactions.csv","w") as reactions:
     for trial in range(num_trials):
         line = s.readline().decode().strip()
-        if trial != 0:
-            reactions.write("\n") #new line
+        if trial != 0: # this code ensures that we don't add a blank newline at the end
+            reactions.write("\n")
         reactions.write(line)
-        
+
 s.close() #serial port should be closed after being opened
 
 
